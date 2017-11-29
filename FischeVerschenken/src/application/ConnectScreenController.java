be24e1fd8dd5612234e94088cc26b1ec673ac690
Fiete Schmidt <f.schmidt.me@web.de>
2017-11-29 20:47:31 +0100

    Gui hinzugefuegt

---
+++ FischeVerschenken/src/application/ConnectScreenController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ConnectScreenController {
+
+    @FXML
+    private TextField TF_IP;
+
+    @FXML
+    private TextField TF_Port;
+
+    @FXML
+    private Button BTN_Connect;
+
+    @FXML
+    private Label LBL_Error;
+
+}
--- FischeVerschenken/src/gamelogic/SchiffMap.java
+++ FischeVerschenken/src/gamelogic/SchiffMap.java
@@ -249,8 +249,6 @@ public class SchiffMap {
 		return true;
 	}
 
-	
-
 	public void showMap() {
 		System.out.println("\t1 2 3 4 5 6 7 8 9 10\n");
 		for (int i = 1; i < 11; i++) {
