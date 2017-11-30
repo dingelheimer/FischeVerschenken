240eb592c187699c26acdc1444c3597db824eb20
Chris-PC\Chris <janchristianhopp@gmail.com>
2017-11-30 11:06:03 +0100

    sleep eingebaut, sollte jetzt evt gehen

--- FischeVerschenken/src/server/ServerThread.java
+++ FischeVerschenken/src/server/ServerThread.java
@@ -9,116 +9,133 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
-public class ServerThread extends Thread {
+public class ServerThread extends Thread
+{
 	private Socket clientSocket;
 	private ClientConnection gameConni;
 	public boolean rdyFlag = false;
 	public boolean playerFlagA;
 
-	public ServerThread(Socket clientSocket) {
+	public ServerThread(Socket clientSocket)
+	{
 		this.clientSocket = clientSocket;
 	}
 
+	@SuppressWarnings("static-access")
 	@Override
-	public void run() {
-		try {
+	public void run()
+	{
+		try
+		{
 			OutputStream out;
-			if (clientSocket.equals(gameConni.playerA.getClientSocket())) {
+			if (clientSocket.equals(gameConni.playerA.getClientSocket()))
+			{
 
 				out = gameConni.playerB.getClientSocket().getOutputStream();
 				playerFlagA = true;
-			} else {
+			}
+			else
+			{
 				out = gameConni.playerA.getClientSocket().getOutputStream();
 				playerFlagA = false;
 			}
 			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
 			InputStream in = clientSocket.getInputStream();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-			while (clientSocket.isConnected()) {
+			while (clientSocket.isConnected())
+			{
 				String msg = reader.readLine();
-				if (msg != null) {
-					if (msg.contains("Schuss")) {
+				if (msg != null)
+				{
+					if (msg.contains("Schuss"))
+					{
 						writer.write(msg);
 						writer.newLine();
 						writer.flush();
-					} else if (msg.contains("Schiffe gesetzt")) {
+					}
+					else if (msg.contains("Schiffe gesetzt"))
+					{
 						rdyFlag = true;
 						boolean notStart = true;
-						if(playerFlagA) {
+						if (playerFlagA)
+						{
 							writer.write("Warte auf anderen Spieler...");
 							writer.newLine();
 							writer.flush();
-							if (!gameConni.playerB.rdyFlag) {
+							while (notStart)
+							{
+								notStart = !gameConni.playerB.rdyFlag;
 								try
 								{
-									this.wait();
+									this.sleep(200);
 								}
 								catch (InterruptedException e)
 								{
-									//e.printStackTrace();
+									// e.printStackTrace();
 								}
 							}
-							else {
-								gameConni.playerB.notify();
-							}
-						}else {
+						}
+						else
+						{
 							writer.write("Warte auf anderen Spieler...");
 							writer.newLine();
 							writer.flush();
-							if (!gameConni.playerA.rdyFlag) {
+							while (notStart)
+							{
+								notStart = !gameConni.playerA.rdyFlag;
 								try
 								{
-									this.wait();
+									this.sleep(200);
 								}
 								catch (InterruptedException e)
 								{
-									//e.printStackTrace();
+									// e.printStackTrace();
 								}
-							}
-							else {
-								gameConni.playerA.notify();
 							}
 						}
 						writer.write("Alle Spieler bereit");
 						writer.newLine();
 						writer.flush();
 					}
-					else if (msg.contains("Treffer")) {
+					else if (msg.contains("Treffer"))
+					{
 						writer.write(msg);
 						writer.newLine();
 						writer.flush();
 					}
-					else if (msg.contains("Du hast verloren!")) {
+					else if (msg.contains("Du hast verloren!"))
+					{
 						writer.write(msg);
 						writer.newLine();
 						writer.flush();
 					}
-					else if (msg.contains("Du bist dran")) {
+					else if (msg.contains("Du bist dran"))
+					{
 						writer.write(msg);
 						writer.newLine();
 						writer.flush();
 					}
 				}
 			}
-		} catch (IOException e) {
+		}
+		catch (IOException e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	private void empfangeKoords(String koords) {
-		int x = koords.charAt(0) - 97;
-		int y = Integer.parseInt(koords.substring(1, 2)) - 1;
-	}
-
-	public ClientConnection getGameConni() {
+	public ClientConnection getGameConni()
+	{
 		return gameConni;
 	}
 
-	public void setGameConni(ClientConnection gameConni) {
+	public void setGameConni(ClientConnection gameConni)
+	{
 		this.gameConni = gameConni;
 	}
 
-	public Socket getClientSocket() {
+	public Socket getClientSocket()
+	{
 		return clientSocket;
 	}
 }
